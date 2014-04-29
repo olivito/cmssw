cb0cda7bd7d43dd80257094b26ed0d4c56da5605
olivito <dominick.olivito@gmail.com>
2014-04-29 15:37:29 +0200

    selector for l1 muons

--- RecoMuon/L2MuonSeedGenerator/src/SealModule.cc
+++ RecoMuon/L2MuonSeedGenerator/src/SealModule.cc
@@ -3,6 +3,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoMuon/L2MuonSeedGenerator/src/L2MuonSeedGenerator.h"
-
+#include "RecoMuon/L2MuonSeedGenerator/src/L1MuonSelector.h"
 
 DEFINE_FWK_MODULE(L2MuonSeedGenerator);
+DEFINE_FWK_MODULE(L1MuonSelector);
+
